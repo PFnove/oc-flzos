90c57429980b75b1e3bc6b1d0ad317bd33256d06
PFnove <55917809+PFnove@users.noreply.github.com>
2022-12-10 23:53:38 +0100

    Add files via upload

--- lib/core/boot.lua
+++ lib/core/boot.lua
@@ -1,7 +1,7 @@
 -- called from /init.lua
 local raw_loadfile = ...
 
-_G._OSVERSION = "FLZos 1.0.0"
+_G._OSVERSION = "OpenOS 1.8.0-snapshot"
 
 -- luacheck: globals component computer unicode _OSVERSION
 local component = component
